fbaa6997ba5832688b577f97a691e36efaa9abb2
Tsuyoshi0304 <85753444+Tsuyoshi0304@users.noreply.github.com>
2021-10-30 16:12:26 +0900

    commentator１０人分とmovie_styleデータをseedで投入

--- app/models/movie_style.rb
+++ app/models/movie_style.rb
@@ -5,7 +5,7 @@ class MovieStyle < ApplicationRecord
 	validates :single_shot, presence: true
 	validates :live, presence: true
 
-  enum short: { within_20_minutes: 1, from_30_to_40_minutes: 2, over_1_hour: 3 }
+  enum length: { within_20_minutes: 1, from_30_to_40_minutes: 2, over_1_hour: 3 }
   enum single_shot: { single_shot_main: 1, half_and_half_single_shot: 2, series_main: 3 }
   enum live: { live_main: 1, half_and_half_live: 2, edit_main: 3 }
 end
---
+++ db/fixtures/02_commentator.rb
@@ -0,0 +1,119 @@
+Commentator.seed do |s|
+  s.id = 1
+	s.name = 'キヨ。'
+	s.sex = 1
+	s.age = 30
+	s.is_forming_a_group = 1
+	s.play_style = 10
+  s.appearance = 1
+	s.vtuber = 0
+	s.movie_style_id = 15
+end
+
+Commentator.seed do |s|
+  s.id = 2
+	s.name = 'レトルト'
+	s.sex = 1
+	s.age = 30
+	s.is_forming_a_group = 1
+	s.play_style = 70
+  s.appearance = 1
+	s.vtuber = 0
+	s.movie_style_id = 18
+end
+
+Commentator.seed do |s|
+  s.id = 3
+	s.name = '牛沢'
+	s.sex = 1
+	s.age = 30
+	s.is_forming_a_group = 1
+	s.play_style = 20
+  s.appearance = 1
+	s.vtuber = 0
+	s.movie_style_id = 18
+end
+
+Commentator.seed do |s|
+  s.id = 4
+	s.name = 'ガッチマン'
+	s.sex = 1
+	s.age = 40
+	s.is_forming_a_group = 1
+	s.play_style = 20
+  s.appearance = 1
+	s.vtuber = 1
+	s.movie_style_id = 18
+end
+
+Commentator.seed do |s|
+  s.id = 5
+	s.name = '赤髪のとも'
+	s.sex = 1
+	s.age = 30
+	s.is_forming_a_group = 1
+	s.play_style = 60
+  s.appearance = 1
+	s.vtuber = 0
+	s.movie_style_id = 26
+end
+
+Commentator.seed do |s|
+  s.id = 6
+	s.name = 'フジ'
+	s.sex = 1
+	s.age = 30
+	s.is_forming_a_group = 1
+	s.play_style = 60
+  s.appearance = 0
+	s.vtuber = 0
+	s.movie_style_id = 15
+end
+
+Commentator.seed do |s|
+  s.id = 7
+	s.name = 'ヒラ'
+	s.sex = 1
+	s.age = 30
+	s.is_forming_a_group = 1
+	s.play_style = 70
+  s.appearance = 1
+	s.vtuber = 1
+	s.movie_style_id = 22
+end
+
+Commentator.seed do |s|
+  s.id = 8
+	s.name = 'こーすけ'
+	s.sex = 1
+	s.age = 30
+	s.is_forming_a_group = 1
+	s.play_style = 50
+  s.appearance = 1
+	s.vtuber = 0
+	s.movie_style_id = 14
+end
+
+Commentator.seed do |s|
+  s.id = 9
+	s.name = 'もこう'
+	s.sex = 1
+	s.age = 30
+	s.is_forming_a_group = 0
+	s.play_style = 50
+  s.appearance = 1
+	s.vtuber = 0
+	s.movie_style_id = 15
+end
+
+Commentator.seed do |s|
+  s.id = 10
+	s.name = '加藤純一'
+	s.sex = 1
+	s.age = 30
+	s.is_forming_a_group = 0
+	s.play_style = 10
+  s.appearance = 1
+	s.vtuber = 0
+	s.movie_style_id = 25
+end
