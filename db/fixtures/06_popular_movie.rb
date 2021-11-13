721e8221e52dc013c18b5c5f4c2b9085021f07d0
Tsuyoshi0304 <85753444+Tsuyoshi0304@users.noreply.github.com>
2021-11-13 23:25:47 +0900

    rails_i18nで翻訳

--- db/fixtures/06_popular_movie.rb
+++ db/fixtures/06_popular_movie.rb
@@ -1,6 +1,30 @@
 PopularMovie.seed do |s|
 	s.id = 1
-	s.movie = 'https://www.youtube.com/embed/GtKNZ-rP1O8'
+	s.movie = 'https://www.youtube.com/embed/KyyeOO9ArFo'
 	s.commentator_id = 1
+end
+
+PopularMovie.seed do |s|
+	s.id = 2
+	s.movie = 'https://www.youtube.com/embed/vtnWZSGx1Jo'
+	s.commentator_id = 2
+end
+
+PopularMovie.seed do |s|
+	s.id = 3
+	s.movie = 'https://www.youtube.com/embed/75gr74VcP8o'
+	s.commentator_id = 3
+end
+
+PopularMovie.seed do |s|
+	s.id = 4
+	s.movie = 'https://www.youtube.com/embed/UFNBgOi6vYs'
+	s.commentator_id = 4
+end
+
+PopularMovie.seed do |s|
+	s.id = 5
+	s.movie = 'https://www.youtube.com/embed/UK3n2ogb_t0'
+	s.commentator_id = 5
 end
 
