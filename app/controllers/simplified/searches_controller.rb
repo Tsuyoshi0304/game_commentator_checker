4642f8e5dbf2df74b863005dd21cb00f33975a10
Tsuyoshi0304 <85753444+Tsuyoshi0304@users.noreply.github.com>
2021-12-15 22:34:13 +0900

    簡易版のパラパラと診断ボタン、結果画面を実装

--- app/controllers/simplified/searches_controller.rb
+++ app/controllers/simplified/searches_controller.rb
@@ -1,7 +1,24 @@
 class Simplified::SearchesController < ApplicationController
-  def new
-  end
+  def new; end
 
   def search
+    @feeling = params['feeling']
+    @famous = params['famous']
+    @vtuber = params['vtuber']
+    @sex = params['sex']
+    @length = params['length']
+    @commentators = search_for(@feeling, @famous, @vtuber, @sex, @length)
+  end
+
+  private
+
+  def search_for(feeling, famous, vtuber, sex, length)
+    movie_style = MovieStyle.where(length: length)
+    Commentator.where(feeling: feeling,
+                      famous: famous,
+                      vtuber: vtuber,
+                      sex: sex,
+                      movie_style_id: movie_style.pluck(:id)
+    )
   end
 end
