2964ae9236e5fbae4f0bfcea38f5584ec447af91
Tsuyoshi0304 <85753444+Tsuyoshi0304@users.noreply.github.com>
2021-12-17 20:06:31 +0900

    スマホレスポンシブと通常版のデザインを一部変更

--- app/javascript/simplified/searches/new.js
+++ app/javascript/simplified/searches/new.js
@@ -1,21 +1,21 @@
 $(window).on('load', function() {
-	$('#slider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		adaptiveHeight: true,
-		autoplaySpeed: 1000
-	});
+	//$('#slider').slick({
+	//	slidesToShow: 1,
+	//	slidesToScroll: 1,
+	//	autoplay: true,
+	//	adaptiveHeight: true,
+	//	autoplaySpeed: 1000
+	//});
 });
 
 $(function() {
-	$('.radio-button').on('click', function() {
-		//radio-buttonクラスをクリック後の関数処理
-		$(this).closest(".q-wrapper").css("display", "none");
-		//質問画面にあたる親要素div(class="q-wrapper")をdisplay:none;にする
-		id = $(this).attr("href");
-		//次の質問idをidに格納
-		$(id).addClass("fit").fadeIn("slow").show();
-		//次の質問にfitをつけて出力。
-	});
+	//$('.radio-button').on('click', function() {
+	//	//radio-buttonクラスをクリック後の関数処理
+	//	$(this).closest(".q-wrapper").css("display", "none");
+	//	//質問画面にあたる親要素div(class="q-wrapper")をdisplay:none;にする
+	//	id = $(this).attr("href");
+	//	//次の質問idをidに格納
+	//	$(id).addClass("fit").fadeIn("slow").show();
+	//	//次の質問にfitをつけて出力。
+	//});
 });
