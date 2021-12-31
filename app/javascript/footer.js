63443344099f12481d5e36ecd32c49d1943eaffa
Tsuyoshi0304 <85753444+Tsuyoshi0304@users.noreply.github.com>
2021-12-31 21:53:03 +0900

    プラポリ、利用規約を実装

--- app/javascript/footer.js
+++ app/javascript/footer.js
@@ -1,6 +1,26 @@
 $(function () {
+	//お問い合わせ
   $("#contact-link").on('click', function(){
-		console.log('aaa');
-	$('#contact-modal').fadeIn('slow').show();
+		$('#contact-modal').fadeIn('slow').show();
+	});
+	//プラポリ
+	$("#privacy-link").on('click', function(){
+		$('#privacy-modal').fadeIn('slow').show();
+	});
+	//利用規約
+	$("#terms-link").on('click', function(){
+		$('#terms-modal').fadeIn('slow').show();
+	});
+	//上の閉じるボタン
+	$('.btn-close').on('click', function(){
+		$('#contact-modal').fadeOut('slow');
+		$('#privacy-modal').fadeOut('slow');
+		$('#terms-modal').fadeOut('slow');
+	});
+	//下の閉じるボタン
+	$('.btn2-close').on('click', function(){
+		$('#contact-modal').fadeOut('slow');
+		$('#privacy-modal').fadeOut('slow');
+		$('#terms-modal').fadeOut('slow');
 	});
 });
