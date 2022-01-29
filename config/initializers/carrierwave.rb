5a93eded407d43da1b845f8d920a68802c697036
Tsuyoshi0304 <85753444+Tsuyoshi0304@users.noreply.github.com>
2022-01-29 23:20:18 +0900

    carrierwaveファイルのバケット名のミスを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,8 +14,8 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1' # 東京はap-northeast-1
     }
-    config.fog_directory  = 'game-commentator-checker' #S3のバケット名
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/game-commentator-checker'
+    config.fog_directory  = 'game-commentator-checker-bucket' #S3のバケット名
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/game-commentator-checker-bucket'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
