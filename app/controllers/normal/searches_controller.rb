9f2551abff8ee8b50a95aa1d874435c9b596f5ca
Tsuyoshi0304 <85753444+Tsuyoshi0304@users.noreply.github.com>
2022-02-07 01:41:01 +0900

    normal controllerのsearch_forメソッドを削除

--- app/controllers/normal/searches_controller.rb
+++ app/controllers/normal/searches_controller.rb
@@ -37,33 +37,4 @@ class Normal::SearchesController < ApplicationController
 
     @commentator_params_hash = @params.permit(:parent_genre_name, :sex, :play_style, :is_forming_a_group, :appearance, :vtuber, :length, :live).to_h
   end
-
-  def search_for(genre_name, sex, play_style, is_forming_a_group, appearance, vtuber, length, live)
-    game_genres = GameGenre.where(genre_name: genre_name)
-    games = Game.where(game_genre_id: game_genres.pluck(:id))
-    playings = Playing.where(game_id: games.pluck(:id))
-    movie_style = MovieStyle.find_by(length: length, live: live)
-    Commentator.where(id: playings.pluck(:commentator_id), 
-                      sex: sex, 
-                      play_style: play_style, 
-                      is_forming_a_group: is_forming_a_group, 
-                      appearance: appearance,
-                      vtuber: vtuber,
-                      movie_style_id: movie_style.id)
-  end
-
-  def similar_search_for(genre_name, play_style, vtuber, sex)
-    game_genres = GameGenre.where(genre_name: genre_name)
-    games = Game.where(game_genre_id: game_genres.pluck(:id))
-    playings = Playing.where(game_id: games.pluck(:id))
-    movie_style = MovieStyle.all
-    Commentator.where(id: playings.pluck(:commentator_id),
-                      play_style: play_style, 
-                      vtuber: vtuber,
-                      sex: sex,
-                      is_forming_a_group: [0,1], 
-                      appearance: [0,1],
-                      movie_style_id: movie_style.pluck(:id)
-    )
-  end
 end
