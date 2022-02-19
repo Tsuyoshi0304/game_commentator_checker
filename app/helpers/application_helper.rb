5fdc83f017cc3b540b0b52d7ef20f81e5953fd83
Tsuyoshi0304 <85753444+Tsuyoshi0304@users.noreply.github.com>
2022-02-19 19:14:42 +0900

    gem meta-tagsの導入

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include NameSearchesHelper
+
+  def default_meta_tags
+    {
+      title: '運命の実況者に出会える | ゲーム実況者診断',
+      reverse: true,
+      description: 'あなたにオススメのゲーム実況者を診断します。2択の質問に答えて運命の実況者を見つけよう!' 
+    }
+  end
 end
